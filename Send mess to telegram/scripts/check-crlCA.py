dd6055e8b0b80ff2235647a5aca67c672fde934d
Klaus <grigorievap@ya.ru>
2022-09-18 20:05:13 +0300

    change scripts

--- Send mess to telegram/scripts/check-crlCA.py
+++ Send mess to telegram/scripts/check-crlCA.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import requests, re, subprocess, configparser, socket
@@ -9,14 +9,14 @@ from datetime import datetime
 workpath = '/opt/scripts/'
 conf = workpath + 'config.ini'
 #crlfile = r"/etc/openvpn/crl.pem"
-# Настройка конфига приложение + получение данных из конфиг-файла (config.ini)
+# Настройка конфига приложение + получение данных из конфиг-файла (cred.ini)
 config = configparser.ConfigParser()
 config.read(conf, encoding = 'utf-8-sig')
-crlfile = str(config.get('Vars', 'crl'))
+crlfile = str(config.get('Vars', 'crlCA'))
 # аргументы
 args = ["openssl", "crl", "-inform", "PEM", "-in", crlfile, "-text", "-noout"]
 # Получаем инфу из списка отзыва
-output = subprocess.check_output(args)
+output = subprocess.check_output(args).decode('utf-8')
 # Поиск даты, до которой действует файлик
 q = re.findall('Next Update[a-zA-Z0-9\:\s]+ GMT', output)
 dt = parse('\n'.join(q).replace('Next Update:', '')).replace(tzinfo=None)
@@ -26,7 +26,7 @@ nucrlday = (dt - now).days
 def format_txt():
     if nucrlday%10 == 1 and (not nucrlday%100 == 11):
         txt = 'день'
-    elif (nucrlday%10 == 2 or nucrlday%10 == 3 or nucrlday%10 == 4) and (not nucrlday%100 == $
+    elif (nucrlday%10 == 2 or nucrlday%10 == 3 or nucrlday%10 == 4) and (not nucrlday%100 == 12) and (not nucrlday%100 == 13) and (not nucrlday%100 == 14):
         txt = 'дня'
     else:
         txt = 'дней'
@@ -34,7 +34,8 @@ def format_txt():
 
 def send_mess_tg(emotxt):
     txt = format_txt()
-    emo = (config.get('Emoji', emotxt)).encode('utf-8')
+    #emo = (config.get('Emoji', emotxt)).encode('utf-8')
+    emo = (config.get('Emoji', emotxt))
     token = str(config.get('Telegram', 'tg_token_mytestit_bot'))
     chat_id = str(config.get('Telegram', 'tg_chat_id_my'))
     fqdn = socket.getfqdn()
@@ -45,8 +46,8 @@ def send_mess_tg(emotxt):
 Server name: *{4}*
 IP server: *{5}*
 """.format(emo, emotxt, nucrlday, txt, fqdn, ip_address)
-    URLFull = "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}&parse_mode=ma$
-    req = requests.get(URLFull, timeout=(3, 3))
+    URLFull = "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}&parse_mode=markdown".format(token, chat_id, message)
+    req = requests.get(URLFull, timeout=(3, 3))	
 
 if nucrlday < 5:
     emotxt = 'DISASTER'
